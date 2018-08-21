43f487716c08f822b39197db6e8482beba656728
Henri Stahl <henrystahl@rocketmail.com>
2018-08-21 12:13:41 +0200

    feature/cars_controller__view/index html only

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,22 +1,29 @@
 class CarsController < ApplicationController
   def index
+    @cars = Car.all
   end
 
   def new
+
   end
 
   def create
+
   end
 
   def show
+
   end
 
   def edit
+
   end
 
   def update
+
   end
 
   def destroy
+
   end
 end
