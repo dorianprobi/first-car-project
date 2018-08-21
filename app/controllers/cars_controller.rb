282d931d6c41dbe1bc6685c234f3ddf9050b29db
JonathanSchroeder <joni_schroeder@web.de>
2018-08-21 12:08:26 +0200

    features/cars_controller and view__edit,update

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -12,11 +12,23 @@ class CarsController < ApplicationController
   end
 
   def edit
+    @car = Car.find(params[:id])
   end
 
   def update
+    @car = Car.find(params[:id])
+    @car.update(user_params)
+
+    redirect_to car_path(@car)
   end
 
   def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:car).permit(:brand, :price)
   end
 end
