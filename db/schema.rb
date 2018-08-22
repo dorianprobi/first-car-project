9cc4f403cb7ccb088b0ac3b64cbd48d5a3844fd8
Dorian Probstmeier <dorian@probstmeier.de>
2018-08-22 14:00:21 +0200

    new migration + caspers frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_124538) do
+ActiveRecord::Schema.define(version: 2018_08_22_100707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2018_08_20_124538) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
+    t.text "description"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
